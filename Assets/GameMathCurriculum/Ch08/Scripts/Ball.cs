ea83ee952cca566b6b5a9e55e550ee567a52ee28
jinag8904 <jinag8904@gmail.com>
2026-04-10 10:42:53 +0900

    따라하기 전

--- Assets/GameMathCurriculum/Ch08/Scripts/Ball.cs
+++ Assets/GameMathCurriculum/Ch08/Scripts/Ball.cs
@@ -8,33 +8,16 @@ public class Ball : MonoBehaviour
     public Status state = Status.Stopped;
 
     public Terrain terrain;
-    private NavMeshAgent agent;
     private Vector3 destination;
 
     private void Awake()
     {
-        destination = transform.position;
-
-        agent = GetComponent<NavMeshAgent>();
-        agent.isStopped = true;
+        //destination = transform.position;
     }
 
     void Update()
     {
         if (state == Status.Dropped)
-        {
-            // 바닥에 내려놓기?
             transform.position = new Vector3(transform.position.x, terrain.SampleHeight(transform.position), transform.position.z);
-
-            agent.SetDestination(destination);
-            agent.isStopped = false;
-
-            if (transform.position == agent.destination)
-            {
-                agent.isStopped = true;
-                agent.SetDestination(transform.position);
-                state = Status.Stopped;
-            }
-        }
     }
 }
